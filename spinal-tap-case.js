4241473490f1f936250217d9e514c2296b6d6357
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-02-18 21:28:28 -0800

    Update spinal-tap-case.js
    Accidentally saved over the original file with a different algorithm so I replaced that incorrect algorithm with the correct one.

--- spinal-tap-case.js
+++ spinal-tap-case.js
@@ -1,31 +1,22 @@
-// freeCodeCamp Intermediate Algorithm Scripting Challenge #13
-// https://www.freecodecamp.org/challenges/sum-all-odd-fibonacci-numbers
+// freeCodeCamp Intermediate Algorithm Scripting Challenge #12
+// https://www.freecodecamp.org/challenges/spinal-tap-case
 
-// Sum All Odd Fibonacci Numbers
-// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
-// The first two numbers in the Fibonacci sequence are 1 and 1.
-// Every additional number in the sequence is the sum of the two previous numbers.
-// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
-function sumFibs(num) {
-    return num;
+// Spinal Tap Case
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+    return str;
 }
 
-sumFibs(4);
+spinalCase('This Is Spinal Tap');
 
 // Solution
-function sumFibs(num) {
-    var prevNumber = 0;
-    var currNumber = 1;
-    var sumOdd = 0;
-
-    while (currNumber <= num) {
-        if (currNumber % 2 !== 0) {
-            sumOdd += currNumber;
-        }
-        currNumber += prevNumber;
-        prevNumber = currNumber - prevNumber;
-    }
-    return sumOdd;
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/\s+|_+/g, "-").toLowerCase();
+    return str;
 }
 
-sumFibs(4);
+spinalCase('This Is Spinal Tap');
